e83901e03f948a27421c3ac4e7e04423c2e4435f
Исаев Никита <96546414+Mazay24@users.noreply.github.com>
2023-03-13 14:33:25 +0300

    Add files via upload

---
+++ Урок8/Урок8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Урок8
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Class<string> str = new Class<string>();
+            Class<int> inter = new Class<int>();
+            Class<double> doubles = new Class<double>();
+
+            str.add("tom", "text", "college");
+            inter.add(5, 10, 20);
+            doubles.add(1.35, 0.66, 122.1);
+        }
+    }
+    class Class<T>
+    {
+        List<T> list = new List<T>();
+
+        public void add(params T[] value)
+        {
+            foreach (T a in value)
+            {
+                list.Add(a);
+            }
+        }
+
+        public void delete(int number)
+        {
+            list.RemoveAt(number);
+        }
+
+        public T search(int number)
+        {
+            return list[number];
+        }
+
+        public int getLenght()
+        {
+            return list.Count;
+
+        }
+    }
+}
+
+    
